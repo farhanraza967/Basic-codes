ef8e9287646801f526397191eb4d83896f485698
Farhan Raza <razafarhan967@gmail.com>
2025-04-23 19:58:34 +0500

    Create info.cpp

---
+++ info.cpp
@@ -0,0 +1,14 @@
+#include<iostream> 
+using namespace std; 
+int main() { 
+    int age;    
+    string name;
+    float height_in_cms, weight_in_kg; 
+    age = 19;    
+    name = "Ahmed Khan"; 
+    height_in_cms = 123.8; 
+    weight_in_kg = 58.7; 
+    cout<<"Name:"<<name<<"\t  Age:"<<age<<endl; 
+    cout<<"Height(cm):"<<height_in_cms<<"\t Weight(kg):"<<weight_in_kg; 
+    return 0;
+}
