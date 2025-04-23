f3b9e01d432e4064e50e41d0a98fd7fc0cb30488
Farhan Raza <razafarhan967@gmail.com>
2025-04-23 19:54:11 +0500

    Create bytes.cpp

---
+++ bytes.cpp
@@ -0,0 +1,10 @@
+#include<iostream> 
+using namespace std; 
+int main(void) 
+{ 
+    cout<<"Integer Bytes="<<sizeof(int); 
+    cout<<"\nDouble Bytes="<<sizeof(double); 
+    cout<<"\nCharacter Bytes="<<sizeof(char); 
+    cout<<"\nBoolean Bytes="<<sizeof(bool); 
+    return 0; 
+} 
