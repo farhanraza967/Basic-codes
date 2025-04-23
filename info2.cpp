d1a8a31c6fa4456b79d3a760651df05c45db1f1c
Farhan Raza <razafarhan967@gmail.com>
2025-04-23 19:59:50 +0500

    Create info2.cpp

---
+++ info2.cpp
@@ -0,0 +1,15 @@
+#include<iostream> 
+using namespace std; 
+int main(){ 
+    int Roll_No = 123, salary = 40000; 
+    float CGPA = 3.2; 
+    double pi = 3.1214, x = 0.012, y; 
+    string enrolment_no = "ned/0145/14-15",name; 
+    char section = 'D'; 
+    bool logical = 1; 
+    cout<< "My Roll No:" <<Roll_No<<"\t Pi="<<CGPA; 
+    cout<< endl<<"Value of y is:"<<y<<endl; 
+    cout<< "Name:"<<name<< endl; 
+    cout << "Enrolment:"<< enrolment_no; 
+    return 0; 
+}
