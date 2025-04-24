ec66dfcd28df79e25a425891192ec6a245630e3a
Farhan Raza <razafarhan967@gmail.com>
2025-04-24 23:16:59 +0500

    Create log.cpp

---
+++ log.cpp
@@ -0,0 +1,10 @@
+#include<iostream> 
+#include<cmath> 
+using namespace std; 
+int main() { 
+double num = 10.3; 
+cout<<endl<<"exp("<<num<<") "<<"= "<<exp(num)<<endl; 
+cout<<endl<<"log("<<num<<") "<<"= "<<log(num)<<endl; 
+cout<<endl<<"log10("<<num<<") "<<"= "<<log10(num)<<endl;   
+return 0; 
+} 
