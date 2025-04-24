f81dbd2a396e27c4a31a5b3eee927ceef2c93a93
Farhan Raza <razafarhan967@gmail.com>
2025-04-24 22:56:27 +0500

    Create arth symbols.cpp

---
+++ arth symbols.cpp
@@ -0,0 +1,17 @@
+#include<iostream> 
+using namespace std; 
+int main() 
+{ 
+int number1; 
+int number2; 
+int result; 
+cout<<"Please enter number1 & number2"; 
+cin>>number1>>number2; 
+result = number1 + number2; 
+result = number1 - number2; 
+result = number1 * number2; 
+result = number1 / number2; 
+result = number1 % number2;  
+cout<<result; 
+return 0; 
+}
