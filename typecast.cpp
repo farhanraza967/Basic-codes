be716f4d27354ab1e671ac54ad944cff4621b883
Farhan Raza <razafarhan967@gmail.com>
2025-04-24 23:06:32 +0500

    Create typecast.cpp

---
+++ typecast.cpp
@@ -0,0 +1,10 @@
+#include<iostream> 
+#include<cmath>   
+using namespace std; 
+int main () { 
+float num1 = -9.5; 
+int num2 = 101; 
+cout<<(int)num1;  
+cout<<endl<<(float)num2/10;    
+return 0; 
+ } 
