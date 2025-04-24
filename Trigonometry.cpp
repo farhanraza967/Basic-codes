2d870f3d44f577f311b62d10dfd7c459a64f73b6
Farhan Raza <razafarhan967@gmail.com>
2025-04-24 23:09:44 +0500

    Create Trigonometry.cpp

---
+++ Trigonometry.cpp
@@ -0,0 +1,17 @@
+#include<iostream> 
+#include<cmath>   
+using namespace std;   
+int main() { 
+const double pi = 3.141592; 
+double angle = pi/6;  
+cout<<endl<<"******** Calculating Trigonometric Ratios 
+********"<<endl; 
+cout<<endl<<"All calculations on Angle "<<angle<<" 
+Radians"<<endl; 
+cout<<endl<<"cos("<<angle<<") "<<"= "<<cos(angle)<<endl;
+cout<<endl<<"sin("<<angle<<") "<<"= "<<sin(angle)<<endl; 
+cout<<endl<<"tan("<<angle<<") "<<"= "<<tan(angle)<<endl; 
+cout<<endl<<"******** Calculations Terminated 
+********"<<endl;   
+return 0; 
+} 
