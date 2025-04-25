9108ebb9adb1681efa33b068b50ecae958625c57
Farhan Raza <razafarhan967@gmail.com>
2025-04-25 21:42:52 +0500

    Create power without function.cpp

---
+++ power without function.cpp
@@ -0,0 +1,17 @@
+#include<iostream> 
+using namespace std; 
+int main() {    
+    int base,exponent,answer,counter; 
+    cout<<"Enter a number(integer):"; 
+    cin>>base; 
+    cout<<"Enter an exponent(integer):"; 
+    cin>>exponent; 
+    answer=1;//running product variable 
+    for(counter=exponent;counter>0;counter=counter-1) 
+    { 
+        answer=answer*base; 
+    } 
+    cout<<"\n"<<base<<" raised to power "<<exponent 
+        <<" = "<<answer; 
+    return 0; 
+}
