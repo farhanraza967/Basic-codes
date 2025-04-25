2f96f560c1777c92b049191c26461e4f3b11dbca
Farhan Raza <razafarhan967@gmail.com>
2025-04-25 21:00:14 +0500

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,35 @@
+#include<iostream> 
+using namespace std; 
+int main(){    
+    double operand1, operand2, result; 
+    char operation; 
+    cout<<"\t***Calculator Program***"; 
+    cout<<"\nEnter the desired expression"
+      <<"with spaces<eg 12.6 + 4.32>"; 
+    cin>>operand1>>operation>>operand2; 
+    if(operation=='+') 
+    { 
+        result=operand1+operand2; 
+    } 
+    if(operation=='-') 
+    { 
+        result=operand1-operand2; 
+    } 
+    if(operation=='*') 
+    { 
+        result=operand1*operand2; 
+    } 
+    if(operation=='/') 
+    { 
+        result=operand1/operand2; 
+    } 
+    if(operation!='+' || operation!='-' || 
+       operation!='*'  || operation!='/' ) 
+    { 
+        cout<<"\nInvalid Operator"; 
+ 
+    } 
+    cout<<"\n\nresult="<<result; 
+ 
+    return 0; 
+}
