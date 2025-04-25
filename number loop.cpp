f7ddde965b86312a754068011349158b522b6981
Farhan Raza <razafarhan967@gmail.com>
2025-04-25 21:39:58 +0500

    Create number loop.cpp

---
+++ number loop.cpp
@@ -0,0 +1,11 @@
+#include<iostream> 
+using namespace std; 
+int main(void) 
+{ 
+    int num; 
+    for(int num=0;num<=10;num++) 
+    { 
+        cout<<"\n num = "<<num; 
+    } 
+    return 0; 
+} 
