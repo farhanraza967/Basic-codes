e8b5002a40e7ff24aa9221643b252e4b5778ee54
Farhan Raza <razafarhan967@gmail.com>
2025-04-27 18:34:33 +0500

    Create GCD.cpp

---
+++ GCD.cpp
@@ -0,0 +1,18 @@
+#include<iostream>  
+using namespace std; 
+int main() 
+{ 
+    int first_num, second_num, gcd; 
+    cout<<"Enter first number: "; 
+    cin>>first_num; 
+    cout<<"Enter second number: "; 
+    cin>>second_num; 
+    for(int i=1;i<=first_num&&i<=second_num; i++) 
+    { 
+        if(first_num%i==0 && second_num%i==0) 
+        { 
+            gcd = i; 
+        } 
+    } 
+    cout<<"Greatest Common Divisor(GCD): "<<gcd<<endl; 
+}
