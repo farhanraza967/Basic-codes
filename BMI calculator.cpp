76987b685cf96ed0de249f946b551a693f59ecd9
Farhan Raza <razafarhan967@gmail.com>
2025-04-28 22:23:23 +0500

    Update BMI calculator.cpp

--- BMI calculator.cpp
+++ BMI calculator.cpp
@@ -2,14 +2,18 @@
 using namespace std; 
 int main() {   
     float weight_in_kg ,height_in_meter ,bmi; 
+    
     cout<<"\t \t **Body mass index (BMI) calculator** \n"; 
     cout<<"\t Calculates an index that indicates"<< 
             " healthy weight distribution\n"; 
     cout<<"Enter your weight in Kgs:  "; 
     cin>> weight_in_kg; 
+    
     cout <<"\nEnter your height in meters:  "; 
     cin>> height_in_meter; 
+    
     bmi=weight_in_kg/(height_in_meter*height_in_meter); 
+    
     cout<<"\nYour BMI value is:"<< bmi; 
     cout<<"\n\n \t\t Standard BMI Values for comparison \n"; 
     cout<<"\n \t\t   Less than 18.5 : Underweight"; 
