4e311a98e2a118d0d996d606842ced6d5bb0ae3d
Farhan Raza <razafarhan967@gmail.com>
2025-05-03 19:41:00 +0500

    Update power.cpp

--- power.cpp
+++ power.cpp
@@ -3,8 +3,7 @@
 using namespace std; 
 int main() { 
 double num1 = 10.3, num2 = 2.0; 
-cout<<endl<<"pow("<<num1<<","<<num2<<") "<<"= 
-"<<pow(num1,num2)<<endl; 
+cout<<endl<<"pow("<<num1<<","<<num2<<") "<<"="<<pow(num1,num2)<<endl; 
 cout<<endl<<"sqrt("<<num1<<") "<<"= "<<sqrt(num1)<<endl; 
 cout<<endl<<"cbrt("<<num1<<") "<<"= "<<cbrt(num1)<<endl;   
 return 0; 
