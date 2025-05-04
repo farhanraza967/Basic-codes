2c1aabdbdb27cd88c943daf148e9b2edd11e86d6
Farhan Raza <razafarhan967@gmail.com>
2025-05-04 20:34:41 +0500

    Create annual interest.cpp

---
+++ annual interest.cpp
@@ -0,0 +1,19 @@
+#include<iostream> 
+using namespace std; 
+int main() 
+double balance, rate; 
+int years; 
+cout<<"What is the starting balance : "; 
+cin>>balance; 
+cout<<"What is the annual interest rate : "; 
+cin>>rate; 
+cout<<"How many years to calculate interest : "; 
+cin>>years; 
+for(int i = 1; i<=years; ++i) 
+{ 
+balance *= rate; 
+} 
+cout<<"After "<<years<<" years, balance will be " 
+<<balance<<endl; 
+return 0; 
+} 
