5b0fd855e82b3b6eab8a30f07b3d138a24884a04
Farhan Raza <razafarhan967@gmail.com>
2025-05-10 00:09:27 +0500

    Update Sides of triangle check.cpp

--- Sides of triangle check.cpp
+++ Sides of triangle check.cpp
@@ -2,22 +2,21 @@
 #include <cmath>
 using namespace std;
 const double E = 1e-9;
-bool isGreater(double x, double y) {
-    return (x - y) > E;
+bool isGreater(double x, double y){
+    return (x-y) > E;
 }
-int main() {
+int main(){
     double a, b, c;
-    cout << "Enter length of three sides: ";
-    cin >> a >> b >> c;
-    if (fabs(a) < E || fabs(b) < E || fabs(c) < E) {
-        cout << "Sides must be non-zero positive values." << endl;
+    cout<< "Enter length of three sides: ";
+    cin>> a >> b >> c;
+    if (fabs(a)<E ||fabs(b)<E || fabs(c)< E){
+        cout<< "Sides must be non-zero positive values." << endl;
         return 1;
     }
-    if (isGreater(a + b, c) && isGreater(a + c, b) && isGreater(b + c, a)) {
-        cout << "They are sides of triangle." << endl;
-    } else {
-        cout << "They are not sides of triangle." << endl;
+    if (isGreater(a + b, c) && isGreater(a + c, b) && isGreater(b + c, a)){
+        cout<< "They are sides of triangle." << endl;
+    } else{
+        cout<< "They are not sides of triangle." << endl;
     }
-
     return 0;
 }
