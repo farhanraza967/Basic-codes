60d346a92096eb81eb9f8db5378b51b48cc1ab65
Farhan Raza <razafarhan967@gmail.com>
2025-05-12 21:01:21 +0500

    Update 3 integers relation.cpp

--- 3 integers relation.cpp
+++ 3 integers relation.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int main() {
-    int num1, num2, num3;
+int main(){
+    int n1, n2, n3;
     cout << "Enter three integers: ";
-    cin >> num1 >> num2 >> num3;
-    int sum = num1 + num2 + num3;
-    double average = sum / 3.0;
-    long long product = 1LL * num1 * num2 * num3;
-    int smallest = min({num1, num2, num3});
-    int largest = max({num1, num2, num3});
-    cout << "Sum = " << sum << endl;
-    cout << "Average = " << average << endl;
-    cout << "Product = " << product << endl;
-    cout << "Smallest = " << smallest << endl;
-    cout << "Largest = " << largest << endl;
+    cin >> n1 >> n2 >> n3;
+    int s = n1 + n2 + n3;
+    double a = s / 3.0;
+    long long p = n1 * n2 * n3;
+    int sm = min({n1, n2, n3});
+    int l = max({n1, n2, n3});
+    cout << "Sum = " << s << endl;
+    cout << "Average = " << a << endl;
+    cout << "Product = " << p << endl;
+    cout << "Smallest = " << sm << endl;
+    cout << "Largest = " << l << endl;
 
     return 0;
 }
