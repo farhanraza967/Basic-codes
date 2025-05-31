9560a872912af1218931af1855678a6ebecdb89d
Farhan Raza <razafarhan967@gmail.com>
2025-05-31 20:13:38 +0500

    Create Precision.cpp

---
+++ Precision.cpp
@@ -0,0 +1,11 @@
+#include<iostream> 
+#include<cmath> 
+#include<iomanip> 
+using namespace std; 
+int main(){ 
+cout<<0.1234567809<<endl; 
+cout<<setprecision(10)<<0.1234567809<<endl; 
+cout<<setprecision(12)<<0.123412341234<<endl; 
+cout<<setprecision(8)<<0.12341234<<endl; 
+return 0;
+}
