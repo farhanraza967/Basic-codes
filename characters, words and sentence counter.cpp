527fb6f2c356baa21898ffc02908f9476d036e0f
Farhan Raza <razafarhan967@gmail.com>
2025-06-01 21:07:41 +0500

    Create characters, words and sentence counter.cpp

---
+++ characters, words and sentence counter.cpp
@@ -0,0 +1,32 @@
+#include<iostream> 
+#include<conio.h> 
+using namespace std; 
+int main(void) { 
+int ch_cnt=0,word_cnt=0,sentence_cnt=0; 
+char chinput; 
+cout<<"\nEnter any text,"<<" press ESC to stop:\n"; 
+chinput=getche(); 
+ while(chinput!=27){       
+ch_cnt=ch_cnt+1; 
+if(chinput==' ') 
+{                
+// ASCII value over here 
+word_cnt=word_cnt+1; 
+} 
+// any condition where a sentence ends 
+if((chinput=='.')||(chinput=='?')) 
+{ 
+sentence_cnt=sentence_cnt+1; 
+word_cnt=word_cnt+1; 
+} 
+if(chinput==13)      
+{ 
+cout<< endl; 
+} 
+chinput=getche(); 
+} 
+ch_cnt=ch_cnt-1;       
+cout<<"\nYou entered "<<ch_cnt<< " character(s), "<<word_cnt<< " word(s) and "<<sentence_cnt<< " sentence(s)."; 
+getch(); 
+return 0; 
+}
