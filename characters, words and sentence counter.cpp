585d77cc1c6a7b8f96a91ef5cecaad8204be6a63
Farhan Raza <razafarhan967@gmail.com>
2025-06-01 21:09:40 +0500

    Update characters, words and sentence counter.cpp

--- characters, words and sentence counter.cpp
+++ characters, words and sentence counter.cpp
@@ -1,24 +1,21 @@
 #include<iostream> 
 #include<conio.h> 
 using namespace std; 
-int main(void) { 
+int main(){ 
 int ch_cnt=0,word_cnt=0,sentence_cnt=0; 
 char chinput; 
 cout<<"\nEnter any text,"<<" press ESC to stop:\n"; 
 chinput=getche(); 
  while(chinput!=27){       
 ch_cnt=ch_cnt+1; 
-if(chinput==' ') 
-{                
+if(chinput==' '){                
 word_cnt=word_cnt+1; 
 } 
-if((chinput=='.')||(chinput=='?')) 
-{ 
+if((chinput=='.')||(chinput=='?')){ 
 sentence_cnt=sentence_cnt+1; 
 word_cnt=word_cnt+1; 
 } 
-if(chinput==13)      
-{ 
+if(chinput==13){ 
 cout<< endl; 
 } 
 chinput=getche(); 
