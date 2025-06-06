f48c24aa6717593ee27b66a461c6540c1fcce137
Farhan Raza <razafarhan967@gmail.com>
2025-06-06 17:02:22 +0500

    Update Min\Max by enhanced loop.cpp

--- Min\Max by enhanced loop.cpp
+++ Min\Max by enhanced loop.cpp
@@ -1,24 +1,13 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-  
-  //add code below this line
+int main(){
 int grades[] = {72, 84, 63, 55, 98};
-int min = grades[0]; //set min to the first element in the array
-
-for (int i : grades) { //enhanced for loop
-  if (i < min) { //if element is less than min
-    min = i; //set min to element that is less
+int min = grades[0]; 
+for(int i : grades) {
+  if(i < min) { 
+    min = i; 
   }
 }
-//elements are not modified so enhanced for loop can be used
-
-cout << "The lowest grade is " << min << endl; //print lowest element
-
-
-  //add code above this line
-  
+cout << "The lowest grade is " << min << endl; 
   return 0;
-  
 }
